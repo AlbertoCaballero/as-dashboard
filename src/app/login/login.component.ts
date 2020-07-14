fb7b58abf7c2be3d2d1a92b9f8c8f8242e69a88e
AlbertoCaballero <albertocaballero80@outlook.com>
2020-07-14 15:49:18 -0500

    Login form data binding and submit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  administrator: string;
+  password: string;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginSubmit() {
+    alert("Admin: " + this.administrator + "\nPass: " + this.password);
   }
 
 }
