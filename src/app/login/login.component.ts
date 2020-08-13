b6637a59d9d318cfba32c1f60e9224494c03439e
AlbertoCaballero <albertocaballero80@outlook.com>
2020-08-13 00:27:45 -0500

    Auto login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { Router } from '@angular/router';
 import { StateService } from '../_services/state.service';
@@ -8,7 +8,7 @@ import { StateService } from '../_services/state.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements AfterViewInit, OnInit {
 
   administrator: string;
   password: string;
@@ -16,7 +16,12 @@ export class LoginComponent implements OnInit {
 
   constructor(public auth: AuthService, private router: Router, private state: StateService) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    if(this.userEmail != null && this.password != null)
+      this.loginSubmit();
+  }
 
   loginSubmit() {
     this.auth.signInEmailPassword(this.administrator, this.password, this);
